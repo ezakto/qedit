52cdcfddb9daca2fbd9c4b906a808af1910584a5
Nicolás Arias <nicolas@nicolasarias.com>
2020-04-07 18:31:34 -0300

    Github page

--- src/plugins/hljs.js
+++
@@ -1,6 +0,0 @@
-import hljs from 'highlight.js';
-
-export default function highlight(code, el, options) {
-  const highlighted = hljs.highlight(options.language, code).value;
-  el.innerHTML = highlighted + '\u200B';
-}
--- src/plugins/prism.js
+++
@@ -1,6 +0,0 @@
-import Prism from 'prismjs';
-
-export default function highlight(code, el, options) {
-  const highlighted = Prism.highlight(code, Prism.languages[options.language], options.language);
-  el.innerHTML = highlighted + '\u200B';
-}
